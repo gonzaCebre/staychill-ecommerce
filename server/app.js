2ddfe4510ca6e182722126358dd680528c49a1d1
gonzacebre <76702750+gonzaCebre@users.noreply.github.com>
2023-03-25 14:50:20 -0300

    subida para deploy

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import fileUpload from "express-fileupload"; //Modulo que nos permite subir imagenes
 import postRoutes from "./routes/posts.routes.js";
 import { dirname, join } from "path";
@@ -9,6 +10,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url)); //Esta linea de codig
 
 //Middlewares
 app.use(express.json()); //Permite entender los jsons
+app.use(cors());
+
 app.use(
   fileUpload({
     useTempFiles: true, //Cuando se suba la imagen, lo sube a una carpeta temporal dentro del proyecto para luego mandarlo a otro servicio
@@ -19,5 +22,9 @@ app.use(express.static(join(__dirname, "../client/build"))); //Esto permite que 
 
 //Rutas
 app.use(postRoutes); //Llama a las rutas
+
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, "../client/build/index.html"));
+}); //Esto permite que como la app se va a servir desde el backend, toda peticion pase por el front
 
 export default app;
