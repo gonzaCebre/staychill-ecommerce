09eab92cc8933cc1228ac3235e03a45c69f91dd5
gonzacebre <76702750+gonzaCebre@users.noreply.github.com>
2023-03-25 15:14:59 -0300

    solucinonado error

--- server/app.js
+++ server/app.js
@@ -20,11 +20,11 @@ app.use(
 );
 app.use(express.static(join(__dirname, "../client/build"))); //Esto permite que el server lea el front
 
+//Rutas
+app.use(postRoutes); //Llama a las rutas
+
 app.get("*", (req, res) => {
   res.sendFile(join(__dirname, "../client/build/index.html"));
 }); //Esto permite que como la app se va a servir desde el backend, toda peticion pase por el front
-
-//Rutas
-app.use(postRoutes); //Llama a las rutas
 
 export default app;
