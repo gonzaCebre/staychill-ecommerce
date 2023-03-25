5d0403291e6e234e401ee5d4e07bdf3ebb34c58a
gonzacebre <76702750+gonzaCebre@users.noreply.github.com>
2023-03-25 16:00:27 -0300

    prueba final final

--- server/app.js
+++ server/app.js
@@ -18,13 +18,13 @@ app.use(
     tempFileDir: "./upload", //Estos archivos temporales los sube a esta carpeta
   })
 );
+//Rutas
+app.use("/api", postRoutes); //Llama a las rutas
+
 app.use(express.static(join(__dirname, "../client/build"))); //Esto permite que el server lea el front
 
 app.get("*", (req, res) => {
   res.sendFile(join(__dirname, "../client/build/index.html"));
 }); //Esto permite que como la app se va a servir desde el backend, toda peticion pase por el front
-
-//Rutas
-app.use(postRoutes); //Llama a las rutas
 
 export default app;
