12d1585bd4ff3ebd6779ade26957507ad82c8a3f
gonzacebre <76702750+gonzaCebre@users.noreply.github.com>
2023-03-25 16:46:32 -0300

    prueba

--- server/app.js
+++ server/app.js
@@ -22,10 +22,10 @@ app.use(
 //Rutas
 app.use(postRoutes); //Llama a las rutas
 
-app.use(express.static(join(__dirname, "../client/build"))); //Esto permite que el server lea el front
+app.use(express.static(join(__dirname, "./client/build"))); //Esto permite que el server lea el front
 
 app.get("*", (req, res) => {
-  res.sendFile(join(__dirname, "../client/build/index.html"));
+  res.sendFile(join(__dirname, "./client/build/index.html"));
 }); //Esto permite que como la app se va a servir desde el backend, toda peticion pase por el front
 
 export default app;
