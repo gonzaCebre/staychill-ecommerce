f723ffbc1f6f5e5576d74dc2488ac0462d797675
gonzacebre <76702750+gonzaCebre@users.noreply.github.com>
2023-03-26 21:41:33 -0300

    commit

--- server/app.js
+++ server/app.js
@@ -24,10 +24,10 @@ app.use(
 
 app.use(express.static(join(__dirname, "../client/build"))); //Esto permite que el server lea el front
 
-/* app.get("*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(join(__dirname, "../client/build/index.html"));
 }); //Esto permite que como la app se va a servir desde el backend, toda peticion pase por el front
- */
+
 //Rutas
 app.use("/api", postRoutes); //Llama a las rutas
 
