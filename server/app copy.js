4c319e6b0e1d7509115273849f176eb4a6fbe18b
gonzacebre <76702750+gonzaCebre@users.noreply.github.com>
2023-03-28 22:48:20 -0300

    commit

---
+++ server/app copy.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import fileUpload from "express-fileupload"; //Modulo que nos permite subir imagenes
+import postRoutes from "./routes/posts.routes.js";
+import { dirname, join } from "path";
+
+import { fileURLToPath } from "url";
+
+const app = express(); //Inicializa el servidor
+const __dirname = dirname(fileURLToPath(import.meta.url)); //Esta linea de codigo nos da cual es la ruta raiz de todo el proyecto
+
+//Middlewares
+app.use(express.json()); //Permite entender los jsons
+app.use(cors({ origin: "*" }));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true, //Cuando se suba la imagen, lo sube a una carpeta temporal dentro del proyecto para luego mandarlo a otro servicio
+    tempFileDir: "./upload", //Estos archivos temporales los sube a esta carpeta
+  })
+);
+
+//Rutas
+
+/* app.use(postRoutes); //Llama a las rutas */
+
+app.use(express.static(join(__dirname, "../client/build"))); //Esto permite que el server lea el front
+
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, "../client/build/index.html"));
+}); //Esto permite que como la app se va a servir desde el backend, toda peticion pase por el front
+
+app.use(postRoutes); //Llama a las rutas
+
+export default app;
--- server/app.js
+++ server/app.js
@@ -1,35 +1,29 @@
 import express from "express";
-import cors from "cors";
-import fileUpload from "express-fileupload"; //Modulo que nos permite subir imagenes
-import postRoutes from "./routes/posts.routes.js";
-import { dirname, join } from "path";
-
+import morgan from "morgan";
+import fileUpload from "express-fileupload";
+import path from "path";
+import { dirname } from "path";
 import { fileURLToPath } from "url";
 
-const app = express(); //Inicializa el servidor
-const __dirname = dirname(fileURLToPath(import.meta.url)); //Esta linea de codigo nos da cual es la ruta raiz de todo el proyecto
+import postRoutes from "./routes/posts.routes.js";
 
-//Middlewares
-app.use(express.json()); //Permite entender los jsons
-app.use(cors({ origin: "*" }));
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(
   fileUpload({
-    useTempFiles: true, //Cuando se suba la imagen, lo sube a una carpeta temporal dentro del proyecto para luego mandarlo a otro servicio
-    tempFileDir: "./upload", //Estos archivos temporales los sube a esta carpeta
+    tempFileDir: "./upload",
+    useTempFiles: true,
   })
 );
 
-//Rutas
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-/* app.use(postRoutes); //Llama a las rutas */
+// Routes
+app.use("/api", postRoutes);
 
-app.use(express.static(join(__dirname, "../client/build"))); //Esto permite que el server lea el front
-
-app.get("*", (req, res) => {
-  res.sendFile(join(__dirname, "../client/build/index.html"));
-}); //Esto permite que como la app se va a servir desde el backend, toda peticion pase por el front
-
-app.use(postRoutes); //Llama a las rutas
-
-export default app;
+export { app };
