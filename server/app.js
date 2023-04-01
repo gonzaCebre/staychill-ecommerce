7a35eef8259b300d3227357eadb844b07964b96d
gonzacebre <76702750+gonzaCebre@users.noreply.github.com>
2023-03-31 21:13:34 -0300

    commit

--- server/app.js
+++ server/app.js
@@ -1,18 +1,35 @@
 import express from "express";
 import cors from "cors";
+import axios from "axios";
 import fileUpload from "express-fileupload"; //Modulo que nos permite subir imagenes
 import postRoutes from "./routes/posts.routes.js";
 import { dirname, join } from "path";
-
 import { fileURLToPath } from "url";
+import mercadopago from "mercadopago";
 
 const app = express(); //Inicializa el servidor
-const __dirname = dirname(fileURLToPath(import.meta.url)); //Esta linea de codigo nos da cual es la ruta raiz de todo el proyecto
+
+//Esta linea de codigo nos da cual es la ruta raiz de todo el proyecto
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+mercadopago.configure({
+  access_token:
+    "TEST-7005871603924729-031912-685fe69d8eecd41ee8fc79740ea9dfcd-206611814",
+});
 
 //Middlewares
-app.use(express.json()); //Permite entender los jsons
+/* app.use(express.json());
 app.use(cors({ origin: "*" }));
+app.use(express.urlencoded({ extended: false })); */
+
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(join(__dirname, "../client/build")));
+/* app.use(express.static("../client")); */
+app.use(cors());
+app.get("/", function (req, res) {
+  res.status(200).sendFile("index.html");
+});
 
 app.use(
   fileUpload({
@@ -23,14 +40,15 @@ app.use(
 
 //Rutas
 app.use(postRoutes); //Llama a las rutas
+
 /* app.use(postRoutes); //Llama a las rutas */
-/* 
-app.use(express.static(join(__dirname, "../client/build"))); //Esto permite que el server lea el front
 
-app.get("*", (req, res) => {
+//Esto permite que el server lea el front
+/* app.use(express.static(join(__dirname, "../client/build")));  */
+
+//Esto permite que como la app se va a servir desde el backend, toda peticion pase por el front
+/* app.get("*", (req, res) => {
   res.sendFile(join(__dirname, "../client/build/index.html"));
-}); //Esto permite que como la app se va a servir desde el backend, toda peticion pase por el front
-
- */
+}); */
 
 export default app;
---
+++ server/utils/mercadopago.js
@@ -0,0 +1,9 @@
+import mercadopago from "mercadopago";
+
+mercadopago.configure({
+  access_token: process.env.ACCESS_TOKEN,
+});
+
+module.exports = {
+  mercadopago,
+};
